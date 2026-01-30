68f0f7b79c4829dd86d1810472eff3e9eca45196
jadejabhagyrajsinh123 <Jadejabhagyrajsinh123@gmail.com>
2026-01-30 20:32:02 +0530

    http/middleware:- RoleMiddleware.php

---
+++ laravel/hospital_management_system/app/Http/Middleware/RoleMiddleware.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Auth;
+
+class RoleMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next, string ...$roles): Response
+    {
+        if (!Auth::check()) {
+            return redirect('login');
+        }
+
+        $user = Auth::user();
+
+        if (in_array($user->role, $roles)) {
+            return $next($request);
+        }
+
+        // If user is logged in but doesn't have the role, redirect to their dashboard or show 403
+        return abort(403, 'Unauthorized access.');
+    }
+}
