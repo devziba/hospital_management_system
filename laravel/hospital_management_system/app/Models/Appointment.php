7935c22c36a5dfa4a58c83bd8f7c6f0067da094e
jadejabhagyrajsinh123 <Jadejabhagyrajsinh123@gmail.com>
2026-01-30 20:29:34 +0530

    models:- Appointment.php

---
+++ laravel/hospital_management_system/app/Models/Appointment.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Appointment extends Model
+{
+    use HasFactory;
+
+    protected $guarded = ['id'];
+
+    protected $casts = [
+        'appointment_date' => 'date',
+    ];
+
+    public function patient()
+    {
+        return $this->belongsTo(Patient::class);
+    }
+
+    public function doctor()
+    {
+        return $this->belongsTo(Doctor::class);
+    }
+
+    public function prescription()
+    {
+        return $this->hasOne(Prescription::class);
+    }
+
+    public function invoice()
+    {
+        return $this->hasOne(Invoice::class);
+    }
+}
