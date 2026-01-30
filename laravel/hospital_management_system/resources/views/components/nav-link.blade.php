3be167d45a320b5ebbfb1aab2bc22d43f47d5092
devziba <devwork64@gmail.com>
2026-01-30 21:03:09 +0530

    views/components:- nav-link.blade.php

---
+++ laravel/hospital_management_system/resources/views/components/nav-link.blade.php
@@ -0,0 +1,38 @@
+@props(['route', 'icon'])
+
+@php
+$isActive = request()->routeIs($route);
+$classes = $isActive 
+    ? 'flex items-center px-3 py-2 text-sm font-medium rounded-md bg-blue-50 text-blue-700 group transition-colors' 
+    : 'flex items-center px-3 py-2 text-sm font-medium rounded-md text-gray-700 hover:text-gray-900 hover:bg-gray-50 group transition-colors';
+@endphp
+
+<a href="{{ route($route) }}" {{ $attributes->merge(['class' => $classes]) }}>
+    {{-- Simple SVG Icons mapping based on name passed --}}
+    @if($icon == 'home')
+        <svg class="mr-3 h-5 w-5 {{ $isActive ? 'text-blue-500' : 'text-gray-400 group-hover:text-gray-500' }}" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"/>
+        </svg>
+    @elseif($icon == 'users')
+        <svg class="mr-3 h-5 w-5 {{ $isActive ? 'text-blue-500' : 'text-gray-400 group-hover:text-gray-500' }}" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z" />
+        </svg>
+    @elseif($icon == 'user-group')
+        <svg class="mr-3 h-5 w-5 {{ $isActive ? 'text-blue-500' : 'text-gray-400 group-hover:text-gray-500' }}" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" />
+        </svg>
+    @elseif($icon == 'calendar')
+        <svg class="mr-3 h-5 w-5 {{ $isActive ? 'text-blue-500' : 'text-gray-400 group-hover:text-gray-500' }}" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
+        </svg>
+    @elseif($icon == 'clock')
+        <svg class="mr-3 h-5 w-5 {{ $isActive ? 'text-blue-500' : 'text-gray-400 group-hover:text-gray-500' }}" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
+        </svg>
+    @else
+        <svg class="mr-3 h-5 w-5 {{ $isActive ? 'text-blue-500' : 'text-gray-400 group-hover:text-gray-500' }}" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+        </svg>
+    @endif
+    {{ $slot }}
+</a>
