a29a833caab77163d0ae47c2d05f677d29ac37ac
jadejabhagyrajsinh123 <Jadejabhagyrajsinh123@gmail.com>
2026-01-30 20:25:01 +0530

    models:- medicalRecord.php

---
+++ laravel/hospital_management_system/app/Models/MedicalRecord.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class MedicalRecord extends Model
+{
+    use HasFactory;
+
+    protected $guarded = ['id'];
+
+    protected $casts = [
+        'visit_date' => 'date',
+        'vital_signs' => 'array',
+    ];
+
+    public function patient()
+    {
+        return $this->belongsTo(Patient::class);
+    }
+
+    public function doctor()
+    {
+        return $this->belongsTo(Doctor::class);
+    }
+}
