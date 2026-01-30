8f0829c57d439346e8ed4ab3fd66a8a52ec7704b
jadejabhagyrajsinh123 <Jadejabhagyrajsinh123@gmail.com>
2026-01-30 20:17:28 +0530

    Prescription.php

---
+++ laravel/hospital_management_system/app/Models/Prescription.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Prescription extends Model
+{
+    use HasFactory;
+
+    protected $guarded = ['id'];
+
+    protected $casts = [
+        'medicines' => 'array',
+    ];
+
+    public function appointment()
+    {
+        return $this->belongsTo(Appointment::class);
+    }
+
+    public function patient()
+    {
+        return $this->belongsTo(Patient::class);
+    }
+
+    public function doctor()
+    {
+        return $this->belongsTo(Doctor::class);
+    }
+}
