8b71611d2288f46045c0067fa2233063703a0af6
jadejabhagyrajsinh123 <Jadejabhagyrajsinh123@gmail.com>
2026-01-30 20:19:32 +0530

    models :- Medicine.php

---
+++ laravel/hospital_management_system/app/Models/Medicine.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Medicine extends Model
+{
+    use HasFactory;
+
+    protected $guarded = ['id'];
+
+    protected $casts = [
+        'expiry_date' => 'date',
+        'is_active' => 'boolean',
+    ];
+}
